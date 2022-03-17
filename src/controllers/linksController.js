8567ac6d01f0198129cb7d0fec31e5190188623c
Rayane Ventura <rayyventura@gmail.com>
2022-03-18 00:27:45 +0100

    fix: fixes bugs

--- src/controllers/linksController.js
+++ src/controllers/linksController.js
@@ -5,16 +5,14 @@ export async function postLink(req, res) {
   const { user } = res.locals;
   const url = req.body;
   const shortUrl = shortid.generate();
-  console.log(user);
-  console.log(url);
-  console.log(shortUrl);
+
   try {
-    // await connection.query(
-    //   `
-    //   INSERT INTO urls (url,shortUrl,userId) VALUES ($1, $2, $3)
-    //   `,
-    //   [url, shortUrl, user.id]
-    // );
+    await connection.query(
+      `
+       INSERT INTO urls (url,"shortUrl",userId) VALUES ($1, $2, $3)
+       `,
+      [url, shortUrl, user.id]
+    );
     res.status(201).send({
       shortUrl: shortUrl,
     });
